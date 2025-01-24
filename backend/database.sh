682800c3a2337caa6ef9e99b0385d1ecdc1b10e0
Tomas Barreto <fc56282@alunos.fc.ul.pt>
2025-01-24 10:06:35 +0000

    fix: external postgres container port

--- backend/database.sh
+++ backend/database.sh
@@ -1,2 +1,2 @@
 docker pull postgres
-docker run --name postgres-database -d -p 9000:5432 --network microservices -e POSTGRES_PASSWORD=password postgres
+docker run --name postgres-database -d -p 5432:5432 --network microservices -e POSTGRES_PASSWORD=password postgres
