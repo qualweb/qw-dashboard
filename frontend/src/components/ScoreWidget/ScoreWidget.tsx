e88c79ce0bd4bb23bba083f77fb3309d9db3b462
Tomas Barreto <fc56282@alunos.fc.ul.pt>
2025-03-07 15:31:04 +0000

    add: score widget

--- frontend/src/components/ScoreWidget/ScoreWidget.tsx
+++ frontend/src/components/ScoreWidget/ScoreWidget.tsx
@@ -1,16 +1,51 @@
+import { useEffect, useState } from 'react';
 import './ScoreWidget.css';
 
 import { Progress } from '@ark-ui/react/progress'
 
 function ScoreWidget() {
+    const [value, setValue] = useState(0);
+    const finalValue = 90;
+    
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+        const animationDuration = 1500;
+        const interval = 20;
+        const steps = animationDuration / interval;
+        const increment = finalValue / steps;
+        let currentValue = 0;
+        
+        const timer = setInterval(() => {
+            currentValue += increment;
+            if (currentValue >= finalValue) {
+            setValue(finalValue);
+            clearInterval(timer);
+            } else {
+            setValue(currentValue);
+            }
+        }, interval);
+        
+        return () => clearInterval(timer);
+        }, 300);
+        
+        return () => clearTimeout(timeout);
+    }, []);
+
     return (
-        <div className='score'>
-            <Progress.Root defaultValue={42}>
-                <Progress.Label>Label</Progress.Label>
-                <Progress.ValueText />
-                <Progress.Circle>
-                    <Progress.CircleTrack />
-                    <Progress.CircleRange />
+        <div className="score-container">
+            <h2 className="score-title"><strong>Overall Accessibility Score</strong></h2>
+            <Progress.Root 
+                value={value}
+                min={0} 
+                max={100} 
+                className="score-progress"
+            >
+                <div className="score-value-container">
+                    <Progress.ValueText className="score-value" />
+                </div>
+                <Progress.Circle className="score-circle">
+                    <Progress.CircleTrack className="score-track" />
+                    <Progress.CircleRange className="score-range" />
                 </Progress.Circle>
             </Progress.Root>
         </div>
