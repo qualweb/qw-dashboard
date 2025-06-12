782733bc8527b78d1be65b008ed6f39144d240f8
Tomas Barreto <fc56282@alunos.fc.ul.pt>
2025-06-12 09:54:01 +0100

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,10 +8,7 @@ import App from './App.tsx'
 const AUTH0_DOMAIN=import.meta.env.VITE_AUTH0_DOMAIN
 const AUTH0_CLIENT_ID=import.meta.env.VITE_AUTH0_CLIENT_ID
 
-console.log(AUTH0_DOMAIN)
-console.log(AUTH0_CLIENT_ID)
-
-createRoot(document.getElementById('root')!).render(
+sucreateRoot(document.getElementById('root')!).render(
   <StrictMode>
     { AUTH0_DOMAIN && AUTH0_CLIENT_ID ? (
       <Auth0Provider
