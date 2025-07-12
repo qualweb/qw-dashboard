94a3129b4f90921476136407fc255c471c134ea2
Tomas Barreto <fc56282@alunos.fc.ul.pt>
2025-07-12 15:19:04 +0100

    fix

--- frontend/src/components/IssueLocation/IssueLocation.tsx
+++ frontend/src/components/IssueLocation/IssueLocation.tsx
@@ -16,9 +16,9 @@ function IssueLocation(props: IssueLocationProps) {
 
     return (
         <>
-            <Clipboard.RootProvider value={clipboard} onClick={handleInnerButtonClick} aria-label='Copy selector do clipboard' >
+            <Clipboard.RootProvider value={clipboard} onClick={handleInnerButtonClick} >
                 <Clipboard.Control>
-                    <Clipboard.Trigger className='location-wrapper'>
+                    <Clipboard.Trigger className='location-wrapper' aria-label='Copy selector do clipboard'>
                         <Clipboard.Indicator className='location' copied={<CheckIcon />}>
                             <MapPin />
                         </Clipboard.Indicator>
